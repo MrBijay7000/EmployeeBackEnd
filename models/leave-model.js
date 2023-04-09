46b3c9a6f03ab95edc4390159b3fae5fdf9ebf56
Bijay Shrestha <shresthabzay95@gmail.com>
2023-04-09 10:25:59 +0545

    More models added

---
+++ models/leave-model.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var LeaveSchema = new Schema({
+  employeeId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  appliedDate: { type: Date, required: true },
+  reason: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Leave", LeaveSchema);
---
+++ models/tasks.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  employeeId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  taskgivendate: { type: Date, required: true },
+  status: {
+    type: String,
+    enum: ["Not started", "In progress", "Completed"],
+    default: "Not started",
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
