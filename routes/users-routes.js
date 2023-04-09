2fdec806c1a6c54c3939308d75ed79e438aec249
Bijay Shrestha <shresthabzay95@gmail.com>
2023-04-09 10:21:06 +0545

    Database added

---
+++ routes/users-routes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const adminRoutes = require("./routes/admin-routes");
+const userRoutes = require("./routes/user-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+mongoose.set("strictQuery", false);
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+//   next();
+// });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+  );
+
+  next();
+});
+
+app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://BIJAY:C4EvNJEJvcHeQXvT@cluster0.dka1gve.mongodb.net/users?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5001);
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Not Connected!");
+  });
